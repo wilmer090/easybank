20f17c9b7414ae9732ddb18910801ac7c3700799
Demata <wilmer.demata@accenture.com>
2022-06-17 03:56:02 -1100

    added alt to image

--- src/components/InfoBox.jsx
+++ src/components/InfoBox.jsx
@@ -3,7 +3,7 @@ import "../assets/styles/InfoBox.css";
 const InfoBox = ({ thumbNail, header, text }) => {
   return (
     <div className="infobox_wrapper center-item center-text">
-      <img src={`${thumbNail}`} />
+      <img src={`${thumbNail}`} alt="" />
       <h2 className="light-text">{header}</h2>
       <p>{text}</p>
     </div>
